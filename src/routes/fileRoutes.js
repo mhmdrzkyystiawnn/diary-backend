2dda04422ed7e58bf1e0d971c94b2b70fc1384b2
rizky <gamaugelay524@gmail.com>
2025-12-28 00:33:44 +0700

    Refactor file routes for clarity and simplicity

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -1,19 +1,17 @@
 // File: src/routes/fileRoutes.js
 const express = require('express');
 const router = express.Router();
-const verifyToken = require('../middleware/authMiddleware'); // Satpam
-const upload = require('../middleware/uploadMiddleware');   // Kuli Angkut
-
-// === PERBAIKAN DISINI ===
-// Jangan lupa import getAllFiles juga!
+const verifyToken = require('../middleware/authMiddleware');
+const upload = require('../middleware/uploadMiddleware');
 const { uploadFile, getAllFiles } = require('../controllers/fileController'); 
 
 // Rute:
 
-// 1. GET: Ambil daftar semua file (Buat Dashboard)
+// 1. GET: Ambil daftar file (Endpoint: /api/files)
 router.get('/', verifyToken, getAllFiles); 
 
-// 2. POST: Upload File
-router.post('/upload', verifyToken, upload.single('file'), uploadFile);
+// 2. POST: Upload File (Endpoint: /api/files)
+// GANTI '/upload' JADI '/' AJA BIAR GAK RIBET
+router.post('/', verifyToken, upload.single('file'), uploadFile);
 
 module.exports = router;
